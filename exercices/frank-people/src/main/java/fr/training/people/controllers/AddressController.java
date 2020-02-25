4c818a076923da163c3655039f47a0d730660d4a
Nicolas Issa-Chaverou <nicolas.n.issa-chaverou@renault.com>
2020-02-25 11:34:18 +0100

    add Address controller

--- exercices/frank-people/src/main/java/fr/training/people/controllers/AddressController.java
+++ exercices/frank-people/src/main/java/fr/training/people/controllers/AddressController.java
@@ -1,6 +1,8 @@
 package fr.training.people.controllers;
 
 import fr.training.people.dtos.AddressCreateDto;
+import fr.training.people.services.AddressService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,8 +14,15 @@ import javax.validation.Valid;
 @RequestMapping("/addresses")
 public class AddressController {
 
+  @Autowired // @Inject
+  private AddressService service;
+
+  public AddressController() {
+    // controller
+  }
+
   @PostMapping
   public void create(@RequestBody @Valid AddressCreateDto dto) {
-    System.out.println(dto);
+    service.create( dto );
   }
 }
